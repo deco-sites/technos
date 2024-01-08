71d5407fac81e4f3e6098f4bc43637555b3c128a
Mateus Martins Pinheiro <mateusmartinsmck@gmail.com>
2024-01-08 10:05:23 -0300

    feat: newsletter modal

--- components/footer/NewsletterModal.tsx
+++ components/footer/NewsletterModal.tsx
@@ -5,14 +5,12 @@ import type { ImageWidget } from "apps/admin/widgets.ts";
 import Icon from "$store/components/ui/Icon.tsx";
 import { Picture, Source } from "apps/website/components/Picture.tsx";
 
-
 export interface BackgroundImages {
-    /** @title Imagem inicial de fundo */
+  /** @title Imagem inicial de fundo */
   initial: ImageWidget;
-    /** @title Imagens de fundo após inscrição */
+  /** @title Imagens de fundo após inscrição */
   success: ImageWidget;
 }
-
 
 export interface Backgrounds {
   desktop: BackgroundImages;
@@ -27,7 +25,7 @@ export interface Props {
 function Newsletter(
   { backgrounds }: Props,
 ) {
-  const { desktop, mobile } = backgrounds
+  const { desktop, mobile } = backgrounds;
   const loading = useSignal(false);
   const success = useSignal(false);
   const open = useSignal(true);
@@ -54,7 +52,10 @@ function Newsletter(
         <div class="block">
           <div class="bg-black opacity-50 fixed inset-0 z-40"></div>
           <div class="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full max-h-[550px] max-w-[745px] z-50">
-            <button class="absolute top-[5px] right-0 translate-x-1/2 bg-[#333333] border-none rounded-full p-[0.2rem] cursor-pointer" onClick={() => open.value = false}>
+            <button
+              class="absolute top-[5px] right-0 translate-x-1/2 bg-[#333333] border-none rounded-full p-[0.2rem] cursor-pointer"
+              onClick={() => open.value = false}
+            >
               <Icon id="CloseNewsletter" width={25} height={25} />
             </button>
             <div class="w-full h-full absolute -z-[1]">
@@ -108,13 +109,37 @@ function Newsletter(
                   </Picture>
                 )}
             </div>
-            <form class="flex flex-col items-start justify-center gap-4 relative top-3/4 left-[50px] -translate-x-1/2" onSubmit={handleSubmit}>
-              <input class="p-[5px] w-full max-w-[250px]" type="text" name="firstName" required placeholder="Nome:" />
-              <input class="p-[5px] w-full max-w-[250px]" type="email" name="email" required placeholder="E-mail:" />
-              <button class="border-none py-2 px-4 bg-[#f70000] text-white cursor-pointer font-bold">ASSINAR</button>
+            <form
+              class="flex flex-col items-start justify-center gap-4 relative top-3/4 left-[50px] -translate-x-1/2"
+              onSubmit={handleSubmit}
+            >
+              <input
+                class="p-[5px] w-full max-w-[250px]"
+                type="text"
+                name="firstName"
+                required
+                placeholder="Nome:"
+              />
+              <input
+                class="p-[5px] w-full max-w-[250px]"
+                type="email"
+                name="email"
+                required
+                placeholder="E-mail:"
+              />
+              <button class="border-none py-2 px-4 bg-[#f70000] text-white cursor-pointer font-bold">
+                ASSINAR
+              </button>
               <div class="flex items-center gap-[1ch] text-white">
-                <input class="w-auto p-[5px] max-w-[250px]" type="checkbox" id="newsletter" name="newsletter" />
-                <label for="newsletter">Aceito receber ofertas e novidades por e-mail</label>
+                <input
+                  class="w-auto p-[5px] max-w-[250px]"
+                  type="checkbox"
+                  id="newsletter"
+                  name="newsletter"
+                />
+                <label for="newsletter">
+                  Aceito receber ofertas e novidades por e-mail
+                </label>
               </div>
             </form>
           </div>
